6d784cecbb3415437cf08582d1af8a5dae2bd793
Maui Bartlett <onaepuni@kanaeokana.net>
2025-04-22 22:36:30 -1000

    Update keys in dates.ts to numbers

--- keaolama/src/app/utility/dates.ts
+++ keaolama/src/app/utility/dates.ts
@@ -1,15 +1,15 @@
 
 export const mahinaMap:{[key:string]:string} = {
-    '01': 'Ian',
-    '02': 'Pep',
-    '03': 'Mal',
-    '04': 'ʻApe',
-    '05': 'Mei',
-    '06': 'Iun',
-    '07': 'Iul',
-    '08': 'ʻAuk',
-    '09': 'Kep',
-    '10': 'ʻOka',
-    '11': 'Now',
-    '12': 'Kek'
+    1: 'Ian',
+    2: 'Pep',
+    3: 'Mal',
+    4: 'ʻApe',
+    5: 'Mei',
+    6: 'Iun',
+    7: 'Iul',
+    8: 'ʻAuk',
+    9: 'Kep',
+    10: 'ʻOka',
+    11: 'Now',
+    12: 'Kek'
 }
